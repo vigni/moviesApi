ec33c0a00d04812e2b9362ff4d3cf2a84172936a
Vigni <lucas.vignijr@gmail.com>
2019-11-04 20:47:39 +0100

    structure home

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 import Axios from "axios";
 import "../scss/main.scss";
-  
 
 
-// searchInAPI("avangers");
-// const app = { test: "test"};
-// const app2 = {...app2 };
+
 
 
 
--- src/js/searchApi.js
+++ src/js/searchApi.js
@@ -1,15 +1,53 @@
-const searchInAPI = (search) => {
-    
+const apiKey = "f3644f42368c13e65beb101e19b5849d";
 
+
+const getMoviesByKeyword = (search) => {
 
     const { data } = await request;
 
-    const searchApi = (url, apiKey) => (search ,callback) =>{
+    const results = (url) => (search ,callback) =>{
         const request = axios.get(
-            `http://www.omdbapi.com/?s=${search}$apikey=6191ba54`);
-    
-            request.then(({ data}) => callback(data));
+            `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${search}&page=1`);
+            request.then(({data}) => callback(data));
     }
 
-console.log(results);
+    console.log(results);
 };
+
+
+const getLatestMovies = () => {
+
+    const { data } = await request;
+
+    const results = (url) => (search ,callback) =>{
+        const request = axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&page=1`);
+        request.then(({data}) => callback(data));
+    }
+    console.log(results);
+};
+
+const addMoviesToFavorites = () => {
+
+    const { data } = await request;
+
+    const results = (url) => (search ,callback) =>{
+        const request = axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&page=1`);
+        request.then(({data}) => callback(data));
+    }
+    console.log(results);
+};
+
+
+
+// GET collection of movie by keyword (searchbar)
+//https://developers.themoviedb.org/3/search/search-movies
+
+
+// GET latest movies (default home)
+//https://developers.themoviedb.org/3/discover/movie-discover
+
+
+// POST rating one movie
+//https://developers.themoviedb.org/3/movies/rate-movie
+// https://api.themoviedb.org/3/movie/{movie_id}/rating?api_key=<<api_key>>
+
