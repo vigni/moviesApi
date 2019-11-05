e90e4c680b2f5e6191954c4202c8ef199a82231b
unknown <lucas.vignijr@gmail.com>
2019-11-05 14:45:43 +0100

    filter section

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import Axios from "axios";
 import "../scss/main.scss";
+import "./services/searchApi.js";
 
+const latestMovies = getLatestMovies();
+console.log(latestMovies);
 
-
-
+document.getElementById('output').innerHTML = location.search;
+$(".chosen-select").chosen();
 
 
 document.getElementById("app").innerHTML = "";
--- src/js/services/searchApi.js
+++ src/js/services/searchApi.js
@@ -1,43 +1,26 @@
+import Axios from "axios";
+
 const apiKey = "f3644f42368c13e65beb101e19b5849d";
 
+const getMoviesByKeyword = () => (search ,callback) =>{
+    const request = axios.get(
+    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${search}&page=1`);
+    request.then(({data}) => callback(data));   
+}
 
-const getMoviesByKeyword = (search) => {
-
-    const { data } = await request;
-
-    const results = (url) => (search ,callback) =>{
-        const request = axios.get(
-            `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${search}&page=1`);
-            request.then(({data}) => callback(data));
-    }
-
-    console.log(results);
-};
-
-
-const getLatestMovies = () => {
-
-    const { data } = await request;
-
-    const results = (url) => (search ,callback) =>{
+    const getLatestMovies = () => (callback) =>{
         const request = axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&page=1`);
         request.then(({data}) => callback(data));
     }
-    console.log(results);
-};
 
-const addMoviesToFavorites = () => {
 
-    const { data } = await request;
-
-    const results = (url) => (search ,callback) =>{
+    const addMoviesToFavorites = () => (callback) =>{
         const request = axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&page=1`);
         request.then(({data}) => callback(data));
     }
-    console.log(results);
-};
 
 
+export default searchApi;
 
 // GET collection of movie by keyword (searchbar)
 //https://developers.themoviedb.org/3/search/search-movies
