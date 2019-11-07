3ee7dc6ad03d1d2cc09660de24b90898e5eb1937
Vigni <lucas.vignijr@gmail.com>
2019-11-07 04:08:03 +0100

    img, title and date added on latest film + trying to add rater.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,80 @@
-import Axios from "axios";
+import axios from "axios";
 import "../scss/main.scss";
-import "./services/searchApi.js";
+import {directive} from "@babel/types";
 
-const latestMovies = getLatestMovies();
-console.log(latestMovies);
+//functions service
+// import getLatestMovies from "./services/searchApi";
+import getOneMovie from "./services/searchApi";
+import getLatestMovies from "./services/searchApi";
 
-$('#btn-order').click(function(){
-    $(this).removeClass('active');
-    $('#btn-filter').addClass('active');
 
-});
 
-document.getElementById("app").innerHTML = "";
+const recentMovies = getLatestMovies();
+
+recentMovies("", results => {
+
+    const generateTree = () => {
+        let html ="";
+        
+        results.results.map((element) => {
+
+            // const oneMovie = getOneMovie(element.id);
+            // oneMovie("", results =>{
+            //     console.log(results.results);
+            // });
+
+            var date = element.release_date.split('-');
+            var new_date=  date[2] + '-' + date[1] + '-' + date[0];
+
+            html += '<div class="card">';
+                html += '<div class="row no-gutters">';
+                    html += '<div class="col-md-4">';
+                        html += '<img width="100px" height="200px" id="picture" src="' + 'https://image.tmdb.org/t/p/original/' + element.poster_path + '" class="card-img">';
+                    html += '</div>'
+                    html += '<div class="col-md-8">'
+                        html += '<div class="card-body">';
+                            html += '<h5 class="card-title">' + element.title + '</h5>'
+                            html += '<p class="card-text"><small class="text-muted">'+ new_date +'</small></p>'
+                            html += '<div id="rater"></div>'
+                            html += '<div class="button-section"> <button type="button" class="btn">Voir plus</button></div>'
+                            html += '</div>'
+                    html += '</div>'
+                html += '</div>'
+            html += '</div>'
+        });
+        return html;
+    };
+    
+    document.getElementById("articles").innerHTML = generateTree();
+})
+
+var rater = require("rater-js");
+
+var  myRater  = rater({ element : document.querySelector("#rater"),rateCallback : function rateCallback(rating){      
+    monRater.setRating(notation);
+    monRater.désactiver();
+}}) ;
+
+
+
+
+// const btnorder = document.getElementById("btn-order");
+// const btnfilter = document.getElementById("btn-filter");
+// const bodyorder = document.getElementsByClassName("body-order");
+// const bodyfilter = document.getElementsByClassName("body-filter");
+
+// function activeFilter() {
+//     btnorder.classList.remove('active');
+//     btnfilter.classList.add('active');
+    
+//     bodyorder.remove('active');
+//     bodyfilter.classList.add('active');
+// }
+  
+// function activeOrder() {
+//     btnfilter.classList.remove('active');
+//     btnorder.classList.add('active');
+//     bodyfilter.classList.remove('active');
+//     bodyorder.add('active');
+    
+// }
